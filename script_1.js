d34a403f05f25b5be7c5caf9026dd47dedb4868e
anthopayan <76994877+anthopayan@users.noreply.github.com>
2021-04-06 19:05:15 +0200

    Add files via upload

---
+++ script_1.js
@@ -0,0 +1,47 @@
+const entrepreneurs = [
+    { first: 'Steve', last: 'Jobs', year: 1955 },
+    { first: 'Oprah', last: 'Winfrey', year: 1954 },
+    { first: 'Bill', last: 'Gates', year: 1955 },
+    { first: 'Sheryl', last: 'Sandberg', year: 1969 },
+    { first: 'Mark', last: 'Zuckerberg', year: 1984 },
+    { first: 'Beyonce', last: 'Knowles', year: 1981 },
+    { first: 'Jeff', last: 'Bezos', year: 1964 },
+    { first: 'Diane', last: 'Hendricks', year: 1947 },
+    { first: 'Elon', last: 'Musk', year: 1971 },
+    { first: 'Marissa', last: 'Mayer', year: 1975 },
+    { first: 'Walt', last: 'Disney', year: 1901 },
+    { first: 'Larry', last: 'Page', year: 1973 },
+    { first: 'Jack', last: 'Dorsey', year: 1976 },
+    { first: 'Evan', last: 'Spiegel', year: 1990 },
+    { first: 'Brian', last: 'Chesky', year: 1981 },
+    { first: 'Travis', last: 'Kalanick', year: 1976 },
+    { first: 'Marc', last: 'Andreessen', year: 1971 },
+    { first: 'Peter', last: 'Thiel', year: 1967 }
+  ];
+
+
+function Name() {
+  var tableauName = entrepreneurs.map(obj => {
+    var rObj = {};
+    rObj["firstName"] = obj.first;
+    rObj["lastName"] = obj.last;
+    console.log(rObj);
+  });
+}
+
+function Age() {
+  var tableauAge = entrepreneurs.map(ag => {
+    var rag = {};
+    rag["first"] = ag.first;
+    rag["last"] = ag.last;
+    rag["year"] = (2020 - ag.year);
+    console.log(rag);
+  });
+}
+
+function Birth() {
+  const result = entrepreneurs.filter(element => (element.year > 1969 && element.year < 1980));
+  console.log(result);
+}
+
+
